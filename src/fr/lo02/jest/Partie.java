05f7687ca898765707efc26b328f0dcf5f1011cb
Gabriel <gabriel.ewenczyk@utt.fr>
2025-11-18 19:44:24 +0100

    Partie bien avancée >:3

--- src/fr/lo02/jest/Partie.java
+++ src/fr/lo02/jest/Partie.java
@@ -2,6 +2,11 @@ package fr.lo02.jest;
 
 import java.util.*;
 
+import fr.lo02.jest.enums.Couleur;
+import fr.lo02.jest.enums.Valeur;
+import fr.lo02.jest.regle.attributionTrophees.StrategyTrophee;
+import fr.lo02.jest.regle.attributionTrophees.StrategyTropheeNull;
+
 public class Partie {
 	
 	private static Partie partie = null;
@@ -12,6 +17,8 @@ public class Partie {
 	
 	private Partie() {
 		terminal = new Terminal();
+		joueurs = null;
+		deck = creerDeck();
 	}
 	/**
 	 * Méthode à utiliser pour récupérer le singleton Partie
@@ -34,7 +41,18 @@ public class Partie {
 	 * Crée les joueurs dans l'array joueurs
 	 */
 	public void creerJoueurs() {
-		
+		terminal.afficherChaine("Combien de joueurs y a-t-il ? (3 ou 4)");
+		int saisie = terminal.lireEntier();
+		while (saisie != 3 && saisie != 4) {
+			terminal.afficherChaine("Mauvaise saisie, tapez (3 ou 4)");
+			saisie = terminal.lireEntier();
+		}
+		for (int i = 0; i < saisie; i++) {
+			terminal.afficherChaine("Entrer un nom pour le joueur "+Integer.toString(i)+" : ");
+			String nom = terminal.lireChaine();
+			Joueur j = new Joueur(nom);
+			joueurs.add(j);
+		}
 	}
 	
 	public ArrayList<Joueur> getJoueurs() {
@@ -44,8 +62,26 @@ public class Partie {
 	/**
 	 * Crée les cartes et les mets dans le deck
 	 */
-	public void creerCartes() {
-		
+	public ConteneurCarte creerDeck() {
+		ConteneurCarte cont = new ConteneurCarte();
+		cont.addCarte(new Carte(Valeur.AS, Couleur.CARREAU, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.AS, Couleur.COEUR, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.AS, Couleur.TREFLE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.AS, Couleur.PIQUE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.DEUX, Couleur.CARREAU, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.DEUX, Couleur.COEUR, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.DEUX, Couleur.TREFLE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.DEUX, Couleur.PIQUE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.TROIS, Couleur.CARREAU, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.TROIS, Couleur.COEUR, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.TROIS, Couleur.TREFLE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.TROIS, Couleur.PIQUE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.QUATRE, Couleur.CARREAU, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.QUATRE, Couleur.COEUR, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.QUATRE, Couleur.TREFLE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.QUATRE, Couleur.PIQUE, new StrategyTropheeNull()));
+		cont.addCarte(new Carte(Valeur.JOKER, Couleur.JOKER, new StrategyTropheeNull()));
+		return cont;
 	}
 	
 	public ConteneurCarte getDeck() {
@@ -56,11 +92,26 @@ public class Partie {
 		return stack;
 	}
 	
+	public void joueursPrennentDerniereCarte() {
+		for (Iterator<Joueur> it = joueurs.iterator(); it.hasNext(); ) {
+			Joueur joueur = it.next();
+			Carte c = joueur.getOffre().distribuerCarte();
+			joueur.getJest().addCarte(c);
+		}
+	}
+	
+	public void distribuerTrophees() {
+		
+	}
+	
+	public HashMap<Joueur, Integer> compterScores() {
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		partie = getPartie();
 		
 		partie.creerJoueurs();
-		partie.creerCartes();
 		
 		int round_counter = 0;
 		while (!partie.deck.isEmpty()) {
@@ -72,6 +123,9 @@ public class Partie {
 			round.prendreCartes();
 		}
 		
+		partie.joueursPrennentDerniereCarte();
+		partie.distribuerTrophees();
+		HashMap<Joueur, Integer> scores = partie.compterScores();
 		//TODO finirPartie()
 	}
 
