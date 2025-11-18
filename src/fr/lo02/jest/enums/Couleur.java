512f7af3997f6b56cf0eae4009d9558173356f4a
HugoHugoHugoHugoHugo <105725295+HugoHugoHugoHugoHugo@users.noreply.github.com>
2025-11-18 15:40:50 +0100

    Rajout javadoc enum

--- src/fr/lo02/jest/enums/Couleur.java
+++ src/fr/lo02/jest/enums/Couleur.java
@@ -1,5 +1,9 @@
 package fr.lo02.jest.enums;
-
+/**
+ *Enum des couleur possible pour une carte.
+ *Classé dans l'ordre croissant de priorité.
+ *Possible utilisation de ordinal() pour récupérer la valeur numérique de la couleur de la carte.
+ */
 public enum Couleur {
 	PIQUE,
 	TREFLE,
--- src/fr/lo02/jest/enums/Valeur.java
+++ src/fr/lo02/jest/enums/Valeur.java
@@ -1,5 +1,9 @@
 package fr.lo02.jest.enums;
-
+/**
+ *Enum des valeur possible pour une carte.
+ *Classé dans l'ordre croissant de valeur.
+ *Possible utilisation de ordinal() pour récupérer la valeur numérique de la carte.
+ */
 public enum Valeur {
 	JOKER,
 	AS, 
